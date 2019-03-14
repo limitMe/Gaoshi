fd725143f459c81af35009af5fb48cd9f53a75a8
钟典@Runga <ceo@myaiol.com>
2019-03-14 16:14:47 +0800

    No longer use autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-node app
-echo "Gaoshi is now watching you"
 
-# Add executeable right: $ chmod 777 autorun.sh
-# Go rc.local $ sudo nano /etc/rc.local
-# Add before exit 0 $ su pi -c "exec /home/pi/Gaoshi/autorun.sh"
+# sudo nano /etc/rc.local
+# before exit 0, write
+# sudo node /home/pi/Gaoshi/app.js &
+# & means loop
