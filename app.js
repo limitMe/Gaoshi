d3e7b672c6f76ef6cdc803ccbcb4c5be67ebb8c1
钟典@Runga <ceo@myaiol.com>
2019-03-14 15:45:51 +0800

    Jpegmin not supported by raspberry pi, use jimp instad

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 //Dependencies
 //const PiCamera = require('pi-camera');
 const OSS = require('ali-oss');
-const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
+var Jimp = require('jimp');
 
 const config = require(`${ __dirname }/config.js`);
 
@@ -25,22 +24,23 @@ let client = new OSS({
 myCamera.snap()
   .then((result) => {
     console.log("A picture has been taken")
-    imagemin([`${ __dirname }/test.jpg`], `${ __dirname }/compressed`, {
-      plugins: [
-          imageminJpegtran()
-      ]
-      })
-      .then((result) => {
-        console.log('compression complete');
+    Jimp.read(`${ __dirname }/test.jpg`)
+      .then(result => {
+        return result
+          .resize(256, 256) // resize
+          .quality(60) // set JPEG quality
+          .greyscale() // set greyscale
+          .write(`${ __dirname }/compressed/test.jpg`); // save
+      }).then(result => {
         client.put('name', `${ __dirname }/compressed/test.jpg`)
-        .then(function (r1) {
-          console.log('put success: %j', r1);
-      }).catch(function (err) {
-        console.error('OSS error: %j', err);
+          .then(function (r1) {
+            console.log('put success: %j', r1);
+          }).catch(function (err) {
+            console.error('OSS error: %j', err);
+      })
+      .catch(err => {
+        console.error("Compression error: %j",err);
       });
-    }).catch((error) => {
-        console.log('Compression error: %j', error)
-    })
   })
   .catch((error) => {
     console.log('Camera error: %j', error)
